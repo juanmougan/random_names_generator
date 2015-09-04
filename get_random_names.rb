d59fc7171c142fba61cd3fd2389e3c8b85292483
Juan Manuel Mougan <jmougan@hexacta.com>
2015-09-04 15:48:27 -0300

    Created the layer for Generator and added a 404 page

--- get_random_names.rb
+++ get_random_names.rb
@@ -1,44 +1,41 @@
 require 'sinatra'
 require_relative 'random_name_generator'
 
-class GetRandomNames < Sinatra::Base
-  attr_accessor :random_name_generator
-  
-  def initialize(app = nil, params = {})
-    super(app)
+before do
     @random_name_generator = RandomNameGenerator.new
-  end
-
-  error 404 do
-    "This page could not be found"
-  end
-
-  get '/' do
-    # TODO show some html maybe explain the API o a readme like Github's
-    "Hello World!"
-  end
-
-  # TODO prepend the language in all API calls
-  # like this '/es/name'
-  get '/name' do
-    @random_name_generator.get_random_name()
-  end
-
-  # TODO prepend the language in all API calls
-  # like this '/es/name'
-  get '/last_name' do
-    @random_name_generator.get_random_last_name()
-  end
-
-  # TODO prepend the language in all API calls
-  # like this '/es/name'
-  get '/full_name' do
-    last_name_first = params[:lastnamefirst]
-    if !last_name_first.nil? and "true".casecmp(last_name_first)  #FIXME false should be ignored here
-      "#{@random_name_generator.get_random_last_name()}, #{@random_name_generator.get_random_name()}"
-    else
-      "#{@random_name_generator.get_random_name} #{@random_name_generator.get_random_last_name()}"
-    end
-  end
-
 end
+
+not_found do
+  status 404
+  #erb :oops
+  "This page could not be found"
+end
+
+get '/' do
+  # TODO show some html maybe explain the API o a readme like Github's
+  "Hello World!"
+end
+
+# TODO prepend the language in all API calls
+# like this '/es/name'
+get '/name' do
+  @random_name_generator.get_random_name()
+end
+
+# TODO prepend the language in all API calls
+# like this '/es/name'
+get '/last_name' do
+  @random_name_generator.get_random_last_name()
+end
+
+# TODO prepend the language in all API calls
+# like this '/es/name'
+get '/full_name' do
+  last_name_first = params[:lastnamefirst]
+  if !last_name_first.nil? and "true".casecmp(last_name_first)  #FIXME false should be ignored here
+    "#{@random_name_generator.get_random_last_name()}, #{@random_name_generator.get_random_name()}"
+  else
+    "#{@random_name_generator.get_random_name} #{@random_name_generator.get_random_last_name()}"
+  end
+end
+
