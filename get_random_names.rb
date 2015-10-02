648ad46462281ff3ac15aacc4ec7ff6c758ba687
Juan Manuel Mougan <jmougan@hexacta.com>
2015-10-02 15:35:28 -0300

    started with the json support

--- get_random_names.rb
+++ get_random_names.rb
@@ -1,25 +1,63 @@
 require 'sinatra'
+require 'sinatra/respond_with'
+require 'json'
 require_relative 'random_name_generator'
 
 before do
+    #content_type :json
     @random_name_generator = RandomNameGenerator.new
+end
+
+after do
+  response.body = JSON.dump(response.body)
 end
 
 not_found do
   status 404
   #erb :oops
-  "This page could not be found"
+  { :error => "This page could not be found" }
 end
 
-get '/' do
+#get '/' do
   # TODO show some html maybe explain the API o a readme like Github's
-  "Hello World!"
+#  "Hello World!"
+#end
+
+get '/' do
+  data = { :name => 'example' }
+  puts headers
+  request.accept.each do |type|
+    puts "Type is: #{type}"
+    case type
+    when 'text/html'
+        halt haml(:index, :locals => data)
+        puts 'text/html'
+    when 'application/json'
+        halt data.to_json
+        puts 'text/json'
+    when 'application/atom+xml'
+      halt nokogiri(:'index.atom', :locals => data)
+    when 'application/xml', 'text/xml'
+      halt nokogiri(:'index.xml', :locals => data)
+    when 'text/plain'
+      halt 'just an example'
+    end
+  end
+  puts 'error'
+  error 406
 end
 
 # TODO prepend the language in all API calls
 # like this '/es/name'
 get '/name' do
-  @random_name_generator.get_random_name()
+  #respond_with :index, :name => 'example' do |f|
+  #  f.txt { @random_name_generator.get_random_name() }
+  #end
+  get_name_as_json
+end
+
+def get_name_as_json
+  { :name => @random_name_generator.get_random_name() }
 end
 
 # TODO prepend the language in all API calls
@@ -38,4 +76,3 @@ get '/full_name' do
     "#{@random_name_generator.get_random_name} #{@random_name_generator.get_random_last_name()}"
   end
 end
-
